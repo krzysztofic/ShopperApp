4993ea58902716449bbb4f057d293595812ff51b
krzysztofic <krzysztof.iciek@gmail.com>
2020-11-13 18:21:42 +0100

    Some rename and added new layout with List View plus adapter

--- app/src/main/java/com/shopper/ListsFragment.java
+++ app/src/main/java/com/shopper/ListsFragment.java
@@ -1,8 +1,9 @@
 package com.shopper;
 
+import android.widget.ListView;
+
 import androidx.fragment.app.Fragment;
 
 public class ListsFragment extends Fragment {
-
 
 }
--- app/src/main/java/com/shopper/MainContentFragment.java
+++ app/src/main/java/com/shopper/MainContentFragment.java
@@ -16,7 +16,7 @@ public class MainContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        return inflater.inflate(R.layout.shopping_lists, container, false);
+        return inflater.inflate(R.layout.main_menu, container, false);
     }
 
 
